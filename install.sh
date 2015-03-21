7ba6904aab45d85f302bdb7e8381c99616490a43
Chun-Kai Wang <bunkie.wang@gmail.com>
2015-03-21 01:17:41 -0700

    added the install script

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/zsh
+# This script installs the necessary vim plugins and create symlinks to any desired dotfiles
+
+## Variables
+HOMEDIR=${ZDOTDIR:-$HOME}
+SOURCEDIR=$(dirname $0)
+VIMDIR=$SOURCEDIR/vim
+PREZTODIR=$SOURCEDIR/zprezto
+
+# run necessary submodules
+cd $SOURCEDIR
+git submodule update --init --recursive
+cd -
+
+### SETUP Prezto
+ln -s $PREZTODIR $HOMEDIR/.zprezto
+setopt EXTENDED_GLOB
+for rcfile in "${HOMEDIR}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${HOMEDIR}/.${rcfile:t}"
+done
+
+### SETUP VIM
+# create symlinks
+ln -s $VIMDIR/vimrc $HOMEDIR/.vimrc
+ln -s $VIMDIR/gvimrc $HOMEDIR/.gvimrc
+
+# install vim plugins through Vundle
+vim +PluginInstall +qall
